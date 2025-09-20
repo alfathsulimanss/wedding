a0972674e90812635a76dc6c62162dc71fb11fd3
alfathsulimanss <alfath.suliman@gmail.com>
2025-09-20 15:09:05 +0800

    Update index.blade.php

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -168,7 +168,7 @@
 <!--                             <p class="wow fadeInUp" data-wow-duration="1200ms">Dear {{ $invitation->name }}</p> -->
 <!--                             <p class="wow fadeInUp" data-wow-duration="1200ms">We Are Getting Married {{ \Carbon\Carbon::parse($wedding->reception)->format('d F Y') }}</p> -->
 
-                            <p class="mt-5">Kami Mengundang Anda Untuk Hadir Di Majlis Pernikahan Kami.</p>
+                            <p class="mt-5">Kami Mengundang Anda Untuk Hadir Di Majlis Perkahwinan Kami.</p>
                             <section class="wpo-couple-section section-padding" id="couple">
                                 <div class="container">
                                     <div class="couple-area clearfix">
@@ -284,7 +284,7 @@
                 <span class="shape-2"><img src="{{ url('assets/landing/assets/images/slider/shape2.png') }}" alt=""> </span>
             </h2>
 <!--             <p class="wow fadeInUp" data-wow-duration="1200ms">Dear {{ $invitation->name }}</p> -->
-            <p class="wow fadeInUp" data-wow-duration="1200ms">We Are Getting Married On {{ \Carbon\Carbon::parse($wedding->reception)->format('d F Y') }}</p>
+            <p class="wow fadeInUp" data-wow-duration="1200ms">Our Reception On {{ \Carbon\Carbon::parse($wedding->reception)->format('d F Y') }}</p>
             <div class="row wow fadeInUp" data-wow-duration="1400ms">
                 <div class="col col-xs-12">
                     <div class="clock-grids">
